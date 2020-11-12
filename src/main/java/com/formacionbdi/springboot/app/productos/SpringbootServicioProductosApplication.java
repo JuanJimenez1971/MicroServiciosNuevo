d8134a627c9fc3be159b662fae966076d9e7eb3e
INSAGS\0017005 <juan>
2020-11-12 18:17:54 +0100

    comit inicial

--- src/main/java/com/formacionbdi/springboot/app/productos/SpringbootServicioProductosApplication.java
+++ src/main/java/com/formacionbdi/springboot/app/productos/SpringbootServicioProductosApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//cambio en develop
 public class SpringbootServicioProductosApplication {
 
 	public static void main(String[] args) {
